5d34b9ab52d14a0e3c92dfa6ea3d2d4be0cba829
HipSayu <phamngochiepgamer@gmail.com>
2025-11-28 23:02:40 +0700

    update Module topbar

--- traobang.fe/src/app/layout/component/app.topbar.ts
+++ traobang.fe/src/app/layout/component/app.topbar.ts
@@ -8,12 +8,12 @@ import { LayoutService } from '../service/layout.service';
 import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '@/service/auth.service';
-
+import { PopoverModule } from 'primeng/popover';
 
 @Component({
     selector: 'app-topbar',
     standalone: true,
-    imports: [RouterModule, CommonModule, StyleClassModule, AppConfigurator, MenuModule, ButtonModule],
+    imports: [RouterModule, CommonModule, StyleClassModule, AppConfigurator, MenuModule, ButtonModule, PopoverModule],
     template: ` <div class="layout-topbar">
         <div class="layout-topbar-logo-container">
             <button class="layout-menu-button layout-topbar-action" (click)="layoutService.onMenuToggle()">
@@ -27,6 +27,29 @@ import { AuthService } from '@/service/auth.service';
 
         <div class="layout-topbar-actions">
             <div class="layout-config-menu">
+                <button type="button" class="layout-topbar-action" (click)="op.toggle($event)">
+                    <i class="pi pi-th-large"></i>
+                </button>
+
+                <!--  -->
+                <p-popover #op>
+                    <div class="">
+                        <div class="grid grid-cols-3 gap-4 max-h-[400px] overflow-y-auto">
+                            <div *ngFor="let module of iconModules">
+                                <div
+                                    [ngClass]="{ 'hover:bg-gray-400': layoutService.isDarkTheme(), 'hover:bg-gray-100': !layoutService.isDarkTheme() }"
+                                    class="col-span-1 flex flex-col justify-center items-center p-4 h-24 hover:cursor-pointer  border border-transparent rounded-xl"
+                                    (click)="module.action(); op.hide()"
+                                >
+                                    <i [ngStyle]="module.color" style="font-size: 1.6rem " [ngClass]="module.icon" class="text-6xl text-primary mb-3 "></i>
+                                    <p class="text-sm font-medium text-center truncate ">{{ module.label }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </p-popover>
+                <!--  -->
+
                 <button type="button" class="layout-topbar-action" (click)="toggleDarkMode()">
                     <i [ngClass]="{ 'pi ': true, 'pi-moon': layoutService.isDarkTheme(), 'pi-sun': !layoutService.isDarkTheme() }"></i>
                 </button>
@@ -64,6 +87,7 @@ import { AuthService } from '@/service/auth.service';
                         <i class="pi pi-user"></i>
                         <span>Profile</span>
                     </button> -->
+
                     <button type="button" (click)="menu.toggle($event)" class="layout-topbar-action">
                         <i class="pi pi-user"></i>
                     </button>
@@ -74,8 +98,7 @@ import { AuthService } from '@/service/auth.service';
     </div>`
 })
 export class AppTopbar {
-
-    _authService = inject(AuthService)
+    _authService = inject(AuthService);
 
     items!: MenuItem[];
     overlayMenuItems: MenuItem[] = [
@@ -91,9 +114,33 @@ export class AppTopbar {
             label: 'Đăng xuất',
             icon: 'pi pi-sign-out',
             command: () => {
-                this._authService.logout()
+                this._authService.logout();
             }
         }
+    ];
+
+    iconModules: any = [
+        {
+            label: 'Trao bằng',
+            icon: 'pi pi-trophy',
+            color: { color: 'gold' },
+            url: 'https://traobang.huce.edu.vn/',
+            action: () => window.open('https://traobang.huce.edu.vn/', '_blank')
+        },
+        {
+            label: 'Sơ yếu lý lịch',
+            icon: 'pi pi-id-card',
+            color: {},
+            url: 'https://syll.huce.edu.vn/',
+            action: () => window.open('https://syll.huce.edu.vn/', '_blank')
+        },
+        {
+            label: 'SMS',
+            icon: 'pi pi-send',
+            color: { color: 'black' },
+            url: 'https://sms.huce.edu.vn/',
+            action: () => window.open('https://sms.huce.edu.vn/', '_blank')
+        },
     ];
 
     constructor(public layoutService: LayoutService) {}

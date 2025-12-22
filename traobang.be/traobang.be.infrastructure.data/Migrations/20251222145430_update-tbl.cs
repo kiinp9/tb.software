13b5f3beffb81034e37583a3fda67d837320b494
nghiant <nghiant9608@gmail.com>
2025-12-22 22:08:50 +0700

    migration

---
+++ traobang.be/traobang.be.infrastructure.data/Migrations/20251222145430_update-tbl.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace traobang.be.infrastructure.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatetbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameIndex(
+                name: "IX_Plan",
+                schema: "tb",
+                table: "GiaoDien",
+                newName: "IX_GiaoDien");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdGiaoDien",
+                schema: "tb",
+                table: "Plan",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameIndex(
+                name: "IX_GiaoDien",
+                schema: "tb",
+                table: "GiaoDien",
+                newName: "IX_Plan");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "IdGiaoDien",
+                schema: "tb",
+                table: "Plan",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
--- traobang.be/traobang.be/README.md
+++ traobang.be/traobang.be/README.md
@@ -1,4 +1,4 @@
 ```
 dotnet ef migrations add InitMigration --project traobang.be.infrastructure.data --startup-project traobang.be --output-dir Migrations
-dotnet ef database update --project traobang.be.infrastructure.data --startup-project traobang.be --output-dir Migrations
+dotnet ef database update --project traobang.be.infrastructure.data --startup-project traobang.be
 ```

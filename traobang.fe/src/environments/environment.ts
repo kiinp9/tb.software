78ba3a2e7c4ff9d38d07e3225ed4a0cb89ef228b
nghiant <nghiant9608@gmail.com>
2026-03-11 10:23:30 +0700

    grape license

--- traobang.fe/src/environments/environment.ts
+++ traobang.fe/src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment: IEnvironment = {
     authClientSecret: 'mBSQUHmZ4be5bQYfhwS7hjJZ2zFOCU2e',
     authScope: 'openid offline_access',
     appUrl: 'https://traobang.huce.edu.vn',
-    grapeJsLicense: '',
+    grapeJsLicense: 'e80de4f218a64e5e8bb700503be4d05dd29f6ebce76f4d1e9025a050f0c267f0',
     minioUrl:'https://s3-2.huce.edu.vn:9000'
 };
